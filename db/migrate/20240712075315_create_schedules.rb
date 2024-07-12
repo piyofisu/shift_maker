ac6e5f06cdc16a6f339cf7ddaa051162f5eab9f0
piyofisu <koyapiro.2011.20@gmail.com>
2024-07-12 18:16:32 +0900

    Staffモデル作成

--- db/migrate/20240712075315_create_schedules.rb
+++ db/migrate/20240712075315_create_schedules.rb
@@ -1,8 +1,8 @@
 class CreateSchedules < ActiveRecord::Migration[7.0]
   def change
     create_table :schedules do |t|
-      t.integer :integer      ,null: false
-      t.date    :making_date  ,null: false
+      t.integer :shiftpattern_id  ,null: false
+      t.date    :making_date      ,null: false
       t.timestamps
     end
   end
