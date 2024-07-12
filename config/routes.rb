4e40a2da732abf2eca2874c6a19822ee31a38375
piyofisu <koyapiro.2011.20@gmail.com>
2024-07-12 21:15:00 +0900

    Staff#indexからnew・editへのルート設置

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'schedules#index'
   resources :shedules, only: [:index]
-  resources :staffs, only: [:index, :new]
+  resources :staffs
 end
