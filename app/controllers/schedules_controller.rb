4f5419338adbd6aec153ce4c1c319813cd1c9337
piyofisu <koyapiro.2011.20@gmail.com>
2024-07-17 00:01:01 +0900

    Schedule作成機能・1日ごとの作成

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,14 +1,23 @@
 class SchedulesController < ApplicationController
-  before_action :set_calendar, only: [:index]
+  before_action :set_calendar, only: [:index, :new]
 
   def index
     @staffs = Staff.all
   end
 
   def new
+    @staffs = Staff.all
   end
 
   def create
+    @schedule = Schedule.new(schedule_params)
+    @staff = Staff.find(params[:schedule][:staff_id])
+    @schedule.staffs << @staff
+    if @schedule.save
+      redirect_to new_schedule_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
@@ -19,5 +28,9 @@ class SchedulesController < ApplicationController
   def set_calendar
     @dates = (Date.today.beginning_of_month..Date.today.end_of_month).to_a
     @days = ["日", "月", "火", "水", "木", "金", "土"]
+  end
+
+  def schedule_params
+    params.require(:schedule).permit(:shiftpattern_id, :making_date)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'schedules#index'
-  resources :shedules, only: [:index]
-  resources :staffs
+  resources :schedules, only: [:index, :new, :create, :edit, :update]
+  resources :staffs, except: [:index]
 end
