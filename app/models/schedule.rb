c8cf09f0db2cfb45db9d7b10e74cb5b56e0f0f69
piyofisu <koyapiro.2011.20@gmail.com>
2024-08-03 01:03:03 +0900

    shiftpatternのアソシエーション・バリデーション

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,7 +1,5 @@
 class Schedule < ApplicationRecord
-  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :shiftpattern
-
   has_many :schedule_staffs
   has_many :staffs, through: :schedule_staffs
 end
--- app/models/shiftpattern.rb
+++ app/models/shiftpattern.rb
@@ -1,2 +1,7 @@
 class Shiftpattern < ApplicationRecord
+  has_many  :schedules
+
+  validates :name,        presence: true
+  validates :start_time,  presence: true
+  validates :end_time,    presence: true
 end
