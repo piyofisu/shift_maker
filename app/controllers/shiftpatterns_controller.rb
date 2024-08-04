fdf58bb8b4fd9387918258e9263847bc2a97c1c1
piyofisu <koyapiro.2011.20@gmail.com>
2024-08-04 20:44:14 +0900

    shiftpattern登録機能・ビューファイル編集

--- app/controllers/shiftpatterns_controller.rb
+++ app/controllers/shiftpatterns_controller.rb
@@ -4,4 +4,18 @@ class ShiftpatternsController < ApplicationController
     @shiftpattern = Shiftpattern.new
   end
 
+  def create
+    Shiftpattern.create(shiftpattern_params)
+    redirect_to new_shiftpattern_path
+  end
+
+  def edit
+  end
+  
+
+  private
+
+  def shiftpattern_params
+    params.require(:shiftpattern).permit(:name, :start_time, :end_time)
+  end
 end
--- app/helpers/shiftpatterns_helper.rb
+++ app/helpers/shiftpatterns_helper.rb
@@ -1,2 +1,9 @@
 module ShiftpatternsHelper
+  def calculate_work_duration(start_time, end_time)
+    adjusted_end_time = end_time < start_time ? end_time + 1.day : end_time
+    total_seconds = (adjusted_end_time - start_time).to_i - 1.hour
+    hours = total_seconds / 3600
+    minutes = (total_seconds % 3600) / 60
+    "#{hours}時間#{minutes}分"
+  end
 end
